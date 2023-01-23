45cc8393c1d2d8f7de514704f4eddaf4aa67c7bc
Nuruzzamanshah <mnueuzzaman@gmail.com>
2023-01-23 22:53:41 +0600

    redux store create

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Counter = () => {
+
+  const count =  useSelector( (state) => state.counter.value);
+
     return (
         <div className='card'>
             <div className="card-header bg-secondary text-center">
                 <h4 className='text-white'>My Counter App</h4>
             </div>
             <div className="card-body text-center">
-                <h1>00</h1>
+                <h1>{count}</h1>
                 <div className='my-4'>
                     <button className='btn btn-success m-2'>Increase</button>
                     <button className='btn btn-dark'>Decrease</button>
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+// import { counterSlice } from "../state/counter/counterSlice";
+import counterReducer from "../state/counter/counterSlice";
 
 export default configureStore({
     reducer:{
-        
+        counter:counterReducer
     }
 })
